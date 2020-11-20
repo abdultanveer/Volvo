c68e714491db526a910d2802d2ed2122cee07046
abdul <abdul.tanveer@gmail.com>
2020-11-20 09:00:36 +0530

    button click-- start another activity in other apps

--- app/src/main/java/com/example/volvo/MainActivity.java
+++ app/src/main/java/com/example/volvo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.volvo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,9 +17,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickHandler(View view) {
-        Toast.makeText(this, "android @ volvo", Toast.LENGTH_SHORT).show();
-        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
-        hIntent.putExtra("mykey","abdul ansari");
-        startActivity(hIntent);
+
+        switch (view.getId()){
+            case R.id.buttonLogin:
+               // Toast.makeText(this, "android @ volvo", Toast.LENGTH_SHORT).show();
+                Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+                hIntent.putExtra("mykey","abdul ansari");
+                startActivity(hIntent);
+                break;
+            case R.id.buttonCancel:
+                Intent dIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678"));
+                startActivity(dIntent);
+                break;
+        }
+
     }
 }
