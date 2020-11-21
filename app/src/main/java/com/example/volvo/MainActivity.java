536af53cb59f5c025b11c9e1eb626a2dddbaa9ab
abdul <abdul.tanveer@gmail.com>
2020-11-21 08:04:27 +0530

    activity lifecycle

--- app/src/main/java/com/example/volvo/MainActivity.java
+++ app/src/main/java/com/example/volvo/MainActivity.java
@@ -6,16 +6,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName() ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //layout inflater
+        Log.i(TAG,"oncreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG,"onStart");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onpause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG,"onresume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG,"onstop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG,"ondestroy");
+
     }
 
     public void clickHandler(View view) {
