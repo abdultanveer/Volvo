5922411dd227149086887712014cd12edd2d2e19
abdul <abdul.tanveer@gmail.com>
2020-11-28 15:43:50 +0530

    espresso ui test

---
+++ app/src/androidTest/java/com/example/volvo/LoginTest.java
@@ -0,0 +1,35 @@
+package com.example.volvo;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testLogin(){
+        onView(withId(R.id.editTextName))
+                .perform(typeText("abdul"), closeSoftKeyboard());
+        onView(withId(R.id.buttonLogin)).perform(click());
+        onView(withId(R.id.textViewmain))
+                .check(matches(withText("abdul")));
+
+
+
+    }
+}
